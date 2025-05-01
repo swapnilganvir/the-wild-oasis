34d8d5a474a34b011cb2bfc806c8c23c40e42b8a
Swapnil Ganvir <swapnilganvir55@gmail.com>
2025-05-01 12:22:58 +0530

    Update supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,10 +1,1 @@
-import { createClient } from '@supabase/supabase-js';
-
-export const supabaseUrl = 'https://rciqfmmgaorxfxrhulks.supabase.co';
-
-const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJjaXFmbW1nYW9yeGZ4cmh1bGtzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU0NzQ4NDEsImV4cCI6MjA2MTA1MDg0MX0.U1ZKZM-xaEtA-l-7H0Npg-6lfHFTwpsRWndfjDnQN78';
-
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
+// Removing this intentinally due to privacy reasons
